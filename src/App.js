5d7189929f79938782b0be9244a193179bf2488f
idasoderlund <soderlundida7711@gmail.com>
2025-05-19 14:16:00 +0200

    added post and body functions and css.

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ import "./HeaderComponent/Header.css";
 import "./NavbarComponent/Navbar.css";
 import "./SidebarComponent/Sidebar.css";
 import "./MainComponent/Button";
+import PostList from "./MainComponent/PostList";
+import Post from "./MainComponent/Post";
 
 function GymApp() {
-  const [isLoggedIn, setIsLoggedIn] = useState(true); // Exempel på koniditonell rendering
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
 
   return (
     <div className="app-container">
@@ -21,35 +23,19 @@ function GymApp() {
           <Header />
         </div>
 
-        <div className="main-content">
-          {isLoggedIn ? (
-            <div>
-              <section className="centeredcontent">
-                <h1>Sometimes a good workout is all the therapy you need!</h1>
-              </section>
-              <section>
-                <h2 className="firstparagraph">Workout Schedule:</h2>
-                <p id="scheduleparagraph">
-                  <b>Monday:</b> Full leg day | <b>Tuesday:</b> Back/Shoulders
-                  Workout | <b>Wednesday:</b> Full leg day <br /> <br />
-                  <b>Thursday:</b> Restday! | <b>Friday:</b> Arm/Ab Workout |
-                  <b>Saturday:</b> Full leg day | <b>Sunday:</b> Restday!
-                </p>
-              </section>
-              <section>
-                <h2 className="secondparagraph">Calorie Calculation:</h2>
-                <p id="calorieparagraph">
-                  <b>Daily Calories:</b> 2100 | <b>Daily Proteine Intake:</b>{" "}
-                  158g | <b>Current Deficit/Surplus:</b> +300 Kcal Surplus
-                </p>
-              </section>
-            </div>
-          ) : (
-            <div>
-              <p>Please Sign In to see your daily content.</p>
-            </div>
-          )}
-        </div>
+        {isLoggedIn ? (
+          <div>
+            <section className="centeredcontent">
+              <div className="post-container">
+                <PostList />
+              </div>
+            </section>
+          </div>
+        ) : (
+          <div>
+            <p>Please Sign In to see your daily content.</p>
+          </div>
+        )}
       </div>
     </div>
   );
--- src/MainComponent/PostList.js
+++ src/MainComponent/PostList.js
@@ -6,3 +6,35 @@ flexbox.
 -Component 2 till höger (Se <buttoncomponent)
 
 */
+
+import React from "react";
+import Post from "./Post";
+
+const PostList = () => {
+  const posts = [
+    {
+      id: 1,
+      title: "My First Post",
+      body: "This is the body to my first post.",
+    },
+    {
+      id: 2,
+      title: "My Second Post",
+      body: "This is the body to my second post.",
+    },
+    {
+      id: 3,
+      title: "My Third Post",
+      body: "This is the body to my third post.",
+    },
+  ];
+
+  return (
+    <div>
+      {posts.map((post) => (
+        <Post key={post.id} title={post.title} body={post.body} />
+      ))}
+    </div>
+  );
+};
+export default PostList;
